e1d092e214e4de0c6d223c24cab6491265f23b25
k <kihyuk.sung@outlook.kr>
2021-07-18 20:36:18 +0900

    fix: ex04-04

--- 04/ex04-04.js
+++ 04/ex04-04.js
@@ -1,7 +1,7 @@
 var score; // 변수 선언 (변수 선언문)
 
 /****
- * var는 running execution context 스코프에 접근할 수 있는 변수를 선언한다.
+ * var는 running execution context의 VariableEnvironment 범위의 변수이다.
  * Lexical Environment 가 초기화될 때 생성이 되며
  * undefined로 초기화한다.
  */
