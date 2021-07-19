6f1f710da4c36dca35426133e8c7a3121772ab3f
K <kihyuk.sung@outlook.kr>
2021-07-19 17:33:24 +0900

    feat: ex04-18
    - 여러가지 네이밍 컨벤션
    - js의 경우 변수나 함수 이름은 카멜 케이스를 사용한다.
    - 생성자함수, 클래스의 이름에는 파스칼 케이스를 사용한다.

---
+++ 04/ex04-18.js
@@ -0,0 +1,10 @@
+// 카멜 케이스
+var firstName;
+// 스네이크
+var first_name;
+// 파스칼
+var FirstName; 
+// 헝가리언
+var strFirstName; // type + identifier
+var $elem = document.getElementById('myId'); // DOM 노트
+var observable$ = fromEvent(document, 'click'); // RxJs 옵저버블
