9dae1f32a885fe8292d7b6c73719545a6dc2eb1b
K <kihyuk.sung@outlook.kr>
2021-08-06 21:41:15 +0900

    feat: 1907

---
+++ 19/1907.js
@@ -0,0 +1,7 @@
+const person = {name: 'Lee'};
+
+console.log(person.hasOwnProperty('__proto__'));
+
+console.log(Object.getOwnPropertyDescriptor(Object.prototype, '__proto__'));
+
+console.log({}.__proto__ === Object.prototype);
