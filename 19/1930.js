12e8aa92e816f892f501739f58ba08dbc60cd92f
K <kihyuk.sung@outlook.kr>
2021-08-23 23:15:51 +0900

    feat: to 1935

--- 19/1930.js
+++ 19/1930.js
@@ -6,3 +6,5 @@ const me = new Person('Lee');
 
 console.log(Object.getPrototypeOf(me) === Person.prototype);
 console.log(Object.getPrototypeOf(Person.prototype) === Object.prototype);
+console.log(me.hasOwnProperty('name'));
+console.log(me.foo);
