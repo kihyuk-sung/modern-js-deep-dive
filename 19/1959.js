e33a6b6a287c73fa865da96dc1cf453a8fec5167
K <kihyuk.sung@outlook.kr>
2021-09-16 22:41:17 +0900

    feat: 1962

--- 19/1959.js
+++ 19/1959.js
@@ -10,3 +10,6 @@ console.log('toString' in person);
 
 console.log(Reflect.has(person, 'name'));
 console.log(Reflect.has(person, 'toString'));
+
+console.log(person.hasOwnProperty('name'));
+console.log(person.hasOwnProperty('age'));
