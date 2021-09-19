aff0cff1956eb98c2331692cda11466687b1336b
K <kihyuk.sung@outlook.kr>
2021-09-19 19:52:24 +0900

    feat: 1966

--- 19/1964.js
+++ 19/1964.js
@@ -3,6 +3,11 @@ const person = {
   address: 'Seoul'
 }
 
+console.log('toString' in person); // true
+
 for (const key in person) {
   console.log(`${key}: ${person[key]}`);
 }
+// 이때 toString은 순회하지 않는다
+
+console.log(Object.getOwnPropertyDescriptor(Object.prototype, 'toString'));
