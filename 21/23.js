49310efed7aee16506d0408b24b16894f43218dc
K <kihyuk.sung@outlook.kr>
2021-10-17 11:44:09 +0900

    feat: 2123
    - parseFloat은 문자열을 Float으로 파싱한다.
    - 앞뒤 공백을 무시한다.
    - 공백으로 구분된 문자열은 첫번째 문자만 파싱한다.
    - 그 결과 숫자가 아니면 NaN가 된다.

---
+++ 21/23.js
@@ -0,0 +1,6 @@
+console.log(parseFloat('3.14'));
+console.log(parseFloat('10.00'));
+console.log(parseFloat('34 45 66'));
+console.log(parseFloat('40 years'));
+console.log(parseFloat('He was 40'));
+console.log(parseFloat(' 50  '));
