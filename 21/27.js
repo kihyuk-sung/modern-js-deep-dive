446c5c01894473aa363f427543413e11c35f1b4b
K <kihyuk.sung@outlook.kr>
2021-10-20 13:17:09 +0900

    feat: 2127
    - Number.prototype.toString() 으로 숫자를 문자열로 변환할 수 있다.
    - 인자로 숫자를 넘겨 진수를 정할 수 있다.

---
+++ 21/27.js
@@ -0,0 +1,9 @@
+const x = 15;
+x.toString(2);
+parseInt(x.toString(2), 2);
+x.toString(8);
+parseInt(x.toString(8), 8);
+x.toString(16);
+parseInt(x.toString(16), 16);
+x.toString();
+parseInt(x.toString());
