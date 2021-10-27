26b5c5b50389694a4536508d904dd88b3752f013
K <kihyuk.sung@outlook.kr>
2021-10-27 21:53:46 +0900

    feat: 2139
    - x는 전역변수이므로 delete로 삭제되지 않는다.
    - y는 전역 객체의 프로퍼티이므로, delete로 삭제된다.

---
+++ 21/39.js
@@ -0,0 +1,15 @@
+var x = 10;
+function foo() {
+  y = 20;
+  console.log(x + y);
+}
+
+foo();
+
+console.log(window.x);
+console.log(window.y);
+delete x;
+delete y;
+
+console.log(window.x);
+console.log(window.y);
