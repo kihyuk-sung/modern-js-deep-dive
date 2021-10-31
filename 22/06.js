7a5a809108e9dac664f357bf41af9c0b41b803c0
K <kihyuk.sung@outlook.kr>
2021-10-31 13:32:21 +0900

    feat: 2206
    - this는 함수 호출 시점에서 결정된다.
    - 일반함수, 메서드 호출, 생성자 함수 호출, apply/call/bind 로 나뉜다.

---
+++ 22/06.js
@@ -0,0 +1,15 @@
+const foo = function () {
+  console.dir(this);
+};
+
+foo();
+
+const obj = { foo };
+obj.foo();
+
+new foo();
+
+const bar = { name: 'bar' };
+foo.call(bar);
+foo.apply(bar);
+foo.bind(bar)();
