7ed13455360a87da29d23500d8ae326c3456999a
K <kihyuk.sung@outlook.kr>
2021-12-24 18:37:11 +0900

    feat: 2522
    prototype에 메서드를 추가하는 방법

---
+++ 25/22.js
@@ -0,0 +1,11 @@
+function Person(name) {
+  this.name = name;
+}
+
+Person.prototype.sayHi = function () {
+  console.log(`Hi! My name is ${this.name}`);
+};
+
+const me = new Person('Lee');
+
+me.sayHi();
