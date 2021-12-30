adb5ccc25f9d81a84f9570f9ab87863a82f6eb5c
K <kihyuk.sung@outlook.kr>
2021-12-30 15:11:50 +0900

    feat: 2532
    - 생성자 내에서는 암묵적으로 this가 생성된다.
    - this의 prototype은 Person.prototype이 된다.
    - 생성자는 암묵적으로 this를 반환한다.

---
+++ 25/32.js
@@ -0,0 +1,9 @@
+class Person {
+  constructor(name) {
+    console.log(this);
+    console.log(Object.getPrototypeOf(this) === Person.prototype);
+    this.name = name;
+  }
+}
+
+const me = new Person('Lee');
