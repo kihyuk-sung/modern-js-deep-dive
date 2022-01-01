371feae480eb1a12a8a00a347430880c4064cdb9
K <kihyuk.sung@outlook.kr>
2022-01-01 10:43:49 +0900

    feat: 2536
    클래스 내에서도 접근자 프로퍼티를 사용할 수 있다. 이때 해당 프로퍼티는
    prototype의 프로퍼티가 된다.

---
+++ 25/36.js
@@ -0,0 +1,21 @@
+class Person {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  set fullName(name) {
+    [this.firstName, this.lastName] = name.split(' ');
+  }
+}
+
+const me = new Person('Ungmo', 'Lee');
+
+console.log(`${me.firstName} ${me.lastName}`);
+me.fullName = 'Heegun Lee';
+console.log(me.fullName);
+console.log(Object.getOwnPropertyDescriptor(Person.prototype, 'fullName'));
