c83a4d92ab9fe9130415d2099080c976306e850f
K <kihyuk.sung@outlook.kr>
2022-01-31 18:29:38 +0900

    feat: 2576

--- 25/74.js
+++ 25/74.js
@@ -2,6 +2,9 @@ class Rectangle {
   constructor(width, height) {
     console.log(this);
     console.log(new.target);
+    console.log(Object.getPrototypeOf(this) === ColorRectangle.prototype);
+    console.log(this instanceof ColorRectangle);
+    console.log(this instanceof Rectangle);
     this.width = width;
     this.height = height;
   }
