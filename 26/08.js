8747743c7472365b8f45ae734d63984e30689c05
K <kihyuk.sung@outlook.kr>
2022-02-11 18:14:08 +0900

    feat: 2608
    표준 빌드인 객체가 제공하는 프로토타입 메서드와 정적메서드는 모두
    non-constructor이다

---
+++ 26/08.js
@@ -0,0 +1,6 @@
+console.log(String.prototype.toUpperCase.prototype);
+console.log(String.fromCharCode.prototype);
+console.log(Number.prototype.toFixed.prototype);
+console.log(Number.isFinite.prototype);
+console.log(Array.prototype.map.prototype);
+console.log(Array.from.prototype);
