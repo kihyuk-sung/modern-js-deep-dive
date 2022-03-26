51990cd9b67630ccb13bc21020cbbd9d4363403b
K <kihyuk.sung@outlook.kr>
2022-03-26 09:22:13 +0900

    feat: 2655
    arguments 는 유사 배열이므로, 배열로 변환해서 사용해야했다.

---
+++ 26/55.js
@@ -0,0 +1,8 @@
+function sum() {
+  var array = Array.prototype.slice.call(arguments)
+  return array.reduce(function (pre, cur) {
+    return pre + cur;
+  }, 0);
+}
+
+console.log(sum(1, 2, 3, 4, 5));
