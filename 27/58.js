11c3a6c73b870c9c1f63f401f5f33b811d661c25
K <kihyuk.sung@outlook.kr>
2022-05-31 18:42:26 +0900

    feat: 2758

---
+++ 27/58.js
@@ -0,0 +1,13 @@
+const arr1 = [3, 4];
+arr1.shift(1, 2);
+console.log(arr1);
+
+arr1.push(5, 6);
+console.log(arr1);
+
+const arr2 = [3, 4];
+let result = [1, 2].concat(arr2);
+console.log(result);
+
+result = result.concat(5, 6);
+console.log(result);
