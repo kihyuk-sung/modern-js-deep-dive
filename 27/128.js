ac0ae6f6ebbff1ef9e53de2c7915055d15be7493
K <kihyuk.sung@outlook.kr>
2022-08-09 20:38:01 +0900

    feat: 27128

---
+++ 27/128.js
@@ -0,0 +1,4 @@
+console.log([5, 10, 15].some(item => item > 10));
+console.log([5, 10, 15].some(item => item < 0));
+console.log(['apple', 'banana', 'mango'].some(item => item === 'banana'));
+console.log([].some(item => item > 3));
