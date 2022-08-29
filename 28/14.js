b3ea45e9d5873403b88b94277724414903e9e6d2
K <kihyuk.sung@outlook.kr>
2022-08-29 19:59:40 +0900

    feat: 2814

---
+++ 28/14.js
@@ -0,0 +1,5 @@
+console.log(Number.isFinite(0));
+console.log(Number.isFinite(Number.MAX_VALUE));
+console.log(Number.isFinite(Number.MIN_VALUE));
+console.log(Number.isFinite(Infinity));
+console.log(Number.isFinite(-Infinity));
