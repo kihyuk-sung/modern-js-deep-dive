ac01f7969427c072edf86f56521779af31c3765b
K <kihyuk.sung@outlook.kr>
2022-09-01 18:33:59 +0900

    feat: 2817

---
+++ 28/17.js
@@ -0,0 +1,8 @@
+console.log(Number.isInteger(0));
+console.log(Number.isInteger(123));
+console.log(Number.isInteger(-123));
+console.log(Number.isInteger(0.5));
+console.log(Number.isInteger('123'));
+console.log(Number.isInteger(false));
+console.log(Number.isInteger(Infinity));
+console.log(Number.isInteger(-Infinity));
