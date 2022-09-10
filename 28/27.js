36a43ae1dabaf4263b3029a1cb0b6e0d56a6e106
K <kihyuk.sung@outlook.kr>
2022-09-11 07:37:27 +0900

    feat: 2827

---
+++ 28/27.js
@@ -0,0 +1,4 @@
+console.log((12345.6789).toPrecision() === "12345.6789");
+console.log((12345.6789).toPrecision(1) === "1e+4");
+console.log((12345.6789).toPrecision(2) === "1.2e+4");
+console.log((12345.6789).toPrecision(6) === "12345.7");
