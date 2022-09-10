6d43ae567fac5dc91a6a960b0463d8a5b1042234
K <kihyuk.sung@outlook.kr>
2022-09-10 11:01:51 +0900

    feat: 2826

---
+++ 28/26.js
@@ -0,0 +1,4 @@
+console.log((12345.6789).toFixed() === "12346");
+console.log((12345.6789).toFixed(1) === "12345.7");
+console.log((12345.6789).toFixed(2) === "12345.68");
+console.log((12345.6789).toFixed(3) === "12345.679");
