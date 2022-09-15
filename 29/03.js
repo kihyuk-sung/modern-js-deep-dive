5754a0ee24472bd2cdd365ad0f9811a83e543dfd
K <kihyuk.sung@outlook.kr>
2022-09-15 19:52:06 +0900

    feat: 2903

---
+++ 29/03.js
@@ -0,0 +1,6 @@
+console.log(Math.round(1.4) === 1);
+console.log(Math.round(1.6) === 2);
+console.log(Math.round(-1.4) === -1);
+console.log(Math.round(-1.6) === -2);
+console.log(Math.round(1) === 1);
+console.log(isNaN(Math.round()));
