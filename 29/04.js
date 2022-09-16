d902f4bd47caf7f35588cab73e9b45ce814f0cb0
K <kihyuk.sung@outlook.kr>
2022-09-16 19:48:33 +0900

    feat: 2904

---
+++ 29/04.js
@@ -0,0 +1,6 @@
+console.log(Math.ceil(1.4) === 2);
+console.log(Math.ceil(1.6) === 2);
+console.log(Math.ceil(-1.4) === -1);
+console.log(Math.ceil(-1.6) === -1);
+console.log(Math.ceil(1) === 1);
+console.log(isNaN(Math.ceil()));
