626e9638ed0291c008232fd88788c537c9263019
K <kihyuk.sung@outlook.kr>
2022-09-18 06:51:09 +0900

    feat: 2906

---
+++ 29/06.js
@@ -0,0 +1,6 @@
+console.log(Math.sqrt(9) === 3);
+console.log(isNaN(Math.sqrt(-9)));
+console.log(Math.sqrt(2) === 1.4142135623730951);
+console.log(Math.sqrt(1) === 1);
+console.log(Math.sqrt(0) === 0);
+console.log(isNaN(Math.sqrt()));
