7429f780a305349bac2ff2b70ec80bcd15d7067a
K <kihyuk.sung@outlook.kr>
2022-09-17 11:04:23 +0900

    feat: 2905

---
+++ 29/05.js
@@ -0,0 +1,6 @@
+console.log(Math.floor(1.9) === 1);
+console.log(Math.floor(9.1) === 9);
+console.log(Math.floor(-1.9) === -2);
+console.log(Math.floor(-9.1) === -10);
+console.log(Math.floor(1) === 1);
+console.log(isNaN(Math.floor()));
