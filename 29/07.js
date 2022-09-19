e2966fc363700eb3ad9c457d2c4348a43323d347
K <kihyuk.sung@outlook.kr>
2022-09-19 19:25:00 +0900

    feat: 2907

---
+++ 29/07.js
@@ -0,0 +1,7 @@
+const randomResult = Math.random();
+
+console.log(0 <= randomResult && randomResult < 1);
+
+const random = Math.floor((Math.random() * 10) + 1);
+
+console.log(1 <= random && random <= 10);
