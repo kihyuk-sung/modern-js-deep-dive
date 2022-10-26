da61a36f6df268c2bb4782ff06d5d64f1a94d15e
K <kihyuk.sung@outlook.kr>
2022-10-26 19:13:58 +0900

    feat: 3031

---
+++ 30/31.js
@@ -0,0 +1,7 @@
+const today = new Date('2020/7/24/12:30');
+
+console.log(today.toString());
+console.log(today.toLocaleTimeString());
+console.log(today.toLocaleTimeString('ko-KR'));
+console.log(today.toLocaleTimeString('en-US'));
+console.log(today.toLocaleTimeString('ja-JP'));
