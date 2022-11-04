08cc72a6c84d1f8e7af24024b25acc7171f67de8
K <kihyuk.sung@outlook.kr>
2022-11-04 17:52:33 +0900

    feat: 3108

---
+++ 31/08.js
@@ -0,0 +1,6 @@
+const target  = 'Is this all there is?';
+
+console.log(target.match(/is/));
+console.log(target.match(/is/i));
+console.log(target.match(/is/g));
+console.log(target.match(/is/ig));
