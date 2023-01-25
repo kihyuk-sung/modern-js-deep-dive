96042927438c665f06976de804abd21eb428f7a5
K <kihyuk.sung@outlook.kr>
2023-01-25 18:47:24 +0900

    feat: 3309

---
+++ 33/09.js
@@ -0,0 +1,12 @@
+const Direction = {
+  UP: 1,
+  DOWN: 2,
+  LEFT: 3,
+  RIGHT: 4
+};
+
+const myDirection = Direction.UP;
+
+if (myDirection === Direction.UP) {
+  console.log('You are going up');
+}
