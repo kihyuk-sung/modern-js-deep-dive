cd6e3b0b2b021c1edff8b64860af3b89d281cbf3
K <kihyuk.sung@outlook.kr>
2023-02-03 18:36:26 +0900

    feat: 3402

---
+++ 34/02.js
@@ -0,0 +1,12 @@
+const array = [1, 2, 3];
+
+console.log(Symbol.iterator in array);
+
+for (const item of array) {
+  console.log(item);
+}
+
+console.log([...array]);
+
+const [a, ...rest] = array;
+console.log(a, rest);
