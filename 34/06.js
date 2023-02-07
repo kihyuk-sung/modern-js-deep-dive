7b441441c487c73b01c9a4ab034ada84258293a4
K <kihyuk.sung@outlook.kr>
2023-02-07 19:33:48 +0900

    feat: 3406

---
+++ 34/06.js
@@ -0,0 +1,8 @@
+const array = [1, 2, 3];
+
+const iterator = array[Symbol.iterator]();
+
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
