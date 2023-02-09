c90d460c7aacc54ff50d66f51a61794dd63e7345
K <kihyuk.sung@outlook.kr>
2023-02-09 20:20:27 +0900

    feat: 3408

---
+++ 34/08.js
@@ -0,0 +1,10 @@
+const iterable = [1, 2, 3];
+
+const iterator = iterable[Symbol.iterator]();
+
+for (; ;) {
+  const res = iterator.next();
+  if (res.done) break;
+  const item = res.value;
+  console.log(item);
+}
