07e59a6ea7e0cf07fb066cc5cee0377126080f07
K <kihyuk.sung@outlook.kr>
2023-02-18 20:38:15 +0900

    feat: 3417

---
+++ 34/17.js
@@ -0,0 +1,24 @@
+const fibonacciFunc = function (max) {
+  let [pre, cur] = [0, 1];
+  return {
+    [Symbol.iterator]() { return this; },
+    next() {
+      [pre, cur] = [cur, pre + cur];
+      return { value: cur, done: cur >= max };
+    }
+  };
+};
+
+let iter = fibonacciFunc(10);
+for (num of iter) {
+  console.log(num);
+}
+
+iter = fibonacciFunc(10);
+
+console.log(iter.next());
+console.log(iter.next());
+console.log(iter.next());
+console.log(iter.next());
+console.log(iter.next());
+console.log(iter.next());
