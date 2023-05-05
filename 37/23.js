64c89ab47da324bc0689a5e55423f36afaf7dd2c
K <kihyuk.sung@outlook.kr>
2023-05-05 16:03:28 +0900

    feat: 3723

---
+++ 37/23.js
@@ -0,0 +1,9 @@
+Set.prototype.difference = function (set) {
+  return new Set([...this].filter(v => !set.has(v)));
+};
+
+const setA = new Set([1, 2, 3, 4]);
+const setB = new Set([2, 4]);
+
+console.log(setA.difference(setB));
+console.log(setB.difference(setA));
