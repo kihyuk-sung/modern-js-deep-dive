6a792f737f4ee1e1ad1d45ee71358063011c700f
K <kihyuk.sung@outlook.kr>
2023-05-28 19:40:57 +0900

    feat: 3743

---
+++ 37/43.js
@@ -0,0 +1,10 @@
+const lee = { name: 'Lee' };
+const kim = { name: 'Kim' };
+
+const map = new Map([[lee, 'developer'], [kim, 'designer']]);
+
+console.log(Symbol.iterator in map);
+
+for (const entry of map) {
+  console.log(entry);
+}
