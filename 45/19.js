94f2a3f23607c11bd839c7c3537e674aa13be580
K <kihyuk.sung@outlook.kr>
2023-06-27 21:28:55 +0900

    feat: 4519

---
+++ 45/19.js
@@ -0,0 +1,21 @@
+const promiseGet = url => {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.send();
+    xhr.onload = () => {
+      if (xhr.status == 200) {
+        resolve(JSON.parse(xhr.response));
+      } else {
+        reject(new Error(xhr.status));
+      }
+    };
+  });
+};
+
+const wrongUrl = 'https://jsonplaceholder.typicode.com/XXX/1';
+
+promiseGet(wrongUrl)
+  .then(res => console.log(re))
+  .catch(err => console.error(err))
+  .finally(() => console.log('Bye!'));
